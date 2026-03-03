e25601d1892873d3b555ede24983845f457db7c1
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-03-04 00:08:20 +0500

    Class-03 : Learning pre-increment and post-increment

---
+++ Module-2 Javascript/Class-03/app.js
@@ -0,0 +1,54 @@
+// alert("Welcome to the new class !")
+
+
+// var sum =  3 % 10
+
+// console.log(sum);
+
+
+
+
+
+// / Post increment
+
+// a++;
+
+// Pre increment 
+
+
+// ++a;
+
+
+// console.log(a++);
+
+var a = 10;
+
+
+
+// Pre Decrement / POST DECREMENT
+// console.log(--a);
+// console.log(a++);
+
+
+a = --a + ++a - a++ + ++a + ++a + --a;
+
+console.log(a);
+            
+    //      11 + 12 - 11 + 11 + 11 + 11 - 10
+    //        35
+
+// 1
+
+// 34
+
+// 35
+
+// 16
+
+// -1
+
+// 32
+
+// 21
+
+// 20
