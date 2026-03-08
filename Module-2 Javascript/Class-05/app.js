cfeba7bee20f6a51afdfd751ef67a3dc5493ec75
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-03-08 17:26:42 +0500

    practice JavaScript prompt, string concatenation, and basic conditional logic

---
+++ Module-2 Javascript/Class-05/app.js
@@ -0,0 +1,41 @@
+// var firstName = prompt("Enter your first name:", "Yahya");
+// var lastName = prompt("Enter your last name:", "Iqbal");
+// var fullName = firstName + " " + lastName;
+
+// // console.log(fullName);
+
+// var message = "Thanks, ";
+// var banger = "!";
+
+// var finalAlert = message + fullName + banger;
+
+// alert(finalAlert);
+
+
+// console.log(finalAlert);
+
+
+var question = "Your name?";
+var defaultAnswer = "John Doe";
+
+var spec = prompt(question, defaultAnswer);
+
+alert("You are " + spec);
+
+// var numberOfCats = +prompt("How many cats?");
+
+// console.log(typeof(numberOfCats));
+
+
+// var tooManyCats = numberOfCats + 1;
+// alert("Too many cats! " + tooManyCats); 
+
+
+
+// var favoriteFood = prompt("What is my favorite food?").toLowerCase();
+
+// if(favoriteFood == "biryani"){
+//     alert("You'r correct !");
+// }else{
+//     alert("Wrong answer !");
+// }
