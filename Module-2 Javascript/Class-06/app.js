4870c7f8e96d9436a831dad95fa805ad9a5ed1a7
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-03-09 11:57:18 +0500

    Class-06 : learning if else conditions

---
+++ Module-2 Javascript/Class-06/app.js
@@ -0,0 +1,48 @@
+var num1 = "10";
+var num2 = 10;
+
+// console.log(num1 == num2);
+
+
+// var hero = prompt("Who is your favorite superhero?").toUpperCase();
+
+// if(hero === "BIG BROTHER"){
+//     alert("Correct Answer")
+// }else{
+//     alert("Wrong Answer")
+// }
+
+// var fullName = prompt("Enter your full name:");
+
+
+// if (fullName === "Ali" + " " + "Aftab") {
+//     console.log("Welcome Ali Aftab!");
+    
+//     alert("Welcome Ali Aftab!");
+// }
+
+// console.log("CONTINUE EXECUTION...");
+
+// console.log(!true)
+
+// if(!true){
+//     console.log("This will always execute.");
+// }
+
+var UserEmail = prompt("Enter your email address:");
+
+if(UserEmail !== "a@gmail.com"){
+   console.log("Please signup to continue !");  
+}
+
+
+
+console.log("Welcome back User !");
+
+
+
+// if(UserEmail === "a@gmail.com"){
+//     console.log("Welcome back, user!");
+// }else{
+//     console.log("Please sign up to continue.");
+// }
