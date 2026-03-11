802f080c30b06333360eb4beeb532700daea97a1
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-03-12 00:08:59 +0500

    Class-08 : add exercises for nested if-statements and basic arrays

---
+++ Module-2 Javascript/Class-08/app.js
@@ -0,0 +1,57 @@
+// // NESTED IF statement example
+
+// var condition = false;
+
+// if (condition) {
+//     console.log("OUTER IF RUN !!");
+
+//     if (true) {
+//         console.log("INNER IF RUN !!");
+
+//     }else{
+//         console.log("INNER ELSE RUN !!");
+//     }
+// } else {
+//     console.log("OUTER ELSE RUN !!");
+// }
+
+
+// var userName = prompt("Enter Your Name");
+// var age = prompt("Enter Your Age");
+// var country = prompt("Enter Your Country");
+
+// if(userName){
+//     if(age > 18){
+//         if(country === "Pakistan"){
+//             console.log("The User Name Is " + userName + ", Age Is " + age + ", Country Is " + country);
+//         }
+//     }
+
+// }else{
+//     console.log("You Must Enter Your Name");
+// }
+
+// if(userName && age > 18 && country === "Pakistan"){
+//     console.log("The User Name Is " + userName + ", Age Is " + age + ", Country Is " + country);
+// }
+
+
+
+// var fruits = ["apple", "banana", "orange"];
+
+// console.log(fruits[0]);
+// console.log(fruits.length);
+
+// var userInput = prompt("Enter Fruit Name").toLowerCase();
+
+// if(userInput === fruits[0]){
+//     console.log("Apple Is Found");
+// }else if(userInput === fruits[1]){
+//     console.log("Banana Is Found");
+// }else if(userInput === fruits[2]){
+//     console.log("Orange Is Found");
+// }else{
+//     console.log("Fruit Not Found");
+// }
+
+
