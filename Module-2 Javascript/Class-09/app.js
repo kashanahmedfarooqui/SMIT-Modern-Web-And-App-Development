05e8a5fcb2292fe475b5940e4e6c922ea71842fe
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-03-14 01:49:21 +0500

    Class-09 : Add JavaScript practice for array methods and loop iteration

---
+++ Module-2 Javascript/Class-09/app.js
@@ -0,0 +1,46 @@
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar", "Hyderabad"];
+
+// var citiesLength = cities.length;
+
+// console.log("CitiesLength:", citiesLength);
+
+// console.log("City :", cities[citiesLength - 2]);
+
+
+// cities[6] = "Faisalabad";
+
+// cities[1] = "Multan";
+
+//! SHIFT
+
+// var removedCity = cities.shift();
+// console.log("Removed City:", removedCity);
+
+//! PUSH
+
+// cities.push("Faisalabad", "Multan");
+
+//! POP
+
+// var removedCity = cities.pop();
+// console.log("Removed City:", removedCity);
+
+// ! UNSHIFT
+
+// cities.unshift("Sialkot", "Gujranwala");
+
+// ! SPLICE
+
+// cities.splice(2, 1, "Sargodha");
+// cities.splice(0, 0, "Bahawalpur");
+
+// console.log(cities);
+
+
+// console.log(cities[6]);
+
+
+
+for(var i = 0; i < 500; i++){
+    console.log(i);
+}
