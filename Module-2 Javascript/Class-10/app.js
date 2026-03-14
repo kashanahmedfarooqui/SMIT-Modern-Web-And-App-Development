b39f6d11c8c2f4acace7b907805f27b659418809
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-03-15 00:17:34 +0500

    Class-10 : implementation of for loop

---
+++ Module-2 Javascript/Class-10/app.js
@@ -0,0 +1,32 @@
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar", "Hyderabad"];
+
+
+// var selectedCities = cities.slice(1, 3);
+
+// console.log("CITIES: ", cities);
+
+// console.log("SELECTED CITIES: ", selectedCities);
+
+// selectedCities.pop();
+
+// console.log("AFTER POP - SELECTED CITIES: ", selectedCities);
+
+
+
+//! FOR LOOP
+
+// console.log("CITY : ",cities[0]);
+// console.log("CITY : ",cities[1]);
+// console.log("CITY : ",cities[2]);
+// console.log("CITY : ",cities[3]);
+// console.log("CITY : ",cities[4]);
+// console.log("CITY : ",cities[5]);
+
+// console.log("===================================");
+
+
+
+// for(var i = 0; i < cities.length; i++){
+//     console.log("CITY : ",cities[i]);
+// }
+
