79684823c255e46b06e3f5d9544c36b27da489cf
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-03-30 01:14:50 +0500

    Class-12 : Practicing for loop

---
+++ Module-2 Javascript/Class-12/app.js
@@ -0,0 +1,52 @@
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// var fruits = ["Mango", "Pineapple", "Avacado", "Grapes", "Apple"]
+// var findFruit = prompt("Enter fruit name").toLowerCase()
+
+// var fruitLoop = fruits.length;
+
+// for(var i = 0; i < fruitLoop; i++){
+//     if(findFruit === fruits[i].toLowerCase()){
+//         console.log(fruits[i] + " found on position : " + [i + 1] )
+//         break;
+//     }else{
+//         console.log(fruits[i] + " Not found !")
+//     }
+// }
+
+// var goals = ["Money", "Parahi", "Comeback", "Umrah", "House"];
+
+// for (var i = 0; i < goals.length; i++) {
+//     if ("Parahi" === goals[i]) {
+//         continue;
+//         console.log("Goals 2026: " + goals[i]);
+        
+//     } else {
+//         console.log("Goals 2026: " + goals[i])
+
+//     }
+// } 
+
+
+// for(var i = 1; i <= 5 ; i++){
+//     console.log('=================================');
+    
+//     for(var j = 1; j <=5; j++){
+//         console.log(i + ": " + j)
+//     }
+    
+// }
+
+            //  <=============================== PRACTICE ============================>
+// for(var i=0; i<=5 ; i++){
+//     console.log("Number : ",i);
+// }
+
+for (var i=1; i<=5 ; i++){
+    console.log('============================================');
+    for(var j = 1 ; j <= 5 ; j++){
+        console.log(i +" : " +j);
+    }
+}
