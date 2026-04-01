dd55f47ad479348ec3512c87f18fc678b21cbe81
kashanahmedfarooqui <kashanahmedfarooqui@gmail.com>
2026-04-01 22:25:14 +0500

    Class-15 : Start Learning Functions

---
+++ Module-2 Javascript/Class-15/app.js
@@ -0,0 +1,62 @@
+// parseInt === Number
+
+// var num1 = "10.111212298298129892189189218198928";
+
+// num1 = parseFloat(num1); 
+
+// num1 = num1.toFixed(1);
+
+// // num1 = num1.toString()
+
+// console.log(typeof num1)
+// console.log(num1)
+
+
+//! Function
+
+// Function is a block of code that's perform a specific task 
+
+// function greeting(){
+//     console.log("Hello Ali !");
+// }
+
+// greeting()
+// greeting()
+// greeting()
+
+var guests = ["Ali", "Ahmed", "Waqas", "Asif", "Ahsan", "Huzaifa", "Hassan", "Jamal", "Owais", "Saad", "Saim", "Uzair", "Zain"];
+
+
+function greeting(name, age){ // Receiving Parameter
+    console.log("My name is " + name + " and my age is " + age);
+}
+
+greeting("Ali", 24);
+greeting("Ahmed", 30);
+
+// for(var i = 0; i < guests.length; i++){
+//     if(guests[i] === "Saad"){
+//         console.log("Comeback " + guests[i]);
+//     }else{
+//         greeting(guests[i]); // Passing Argument
+//     }
+        
+// }
+
+
+//! Loop
+
+// Loop is a block code that's perform a repeatitive task until a certain condition is met
+
+// parseInt === Number
+
+// var num1 = "10.111212298298129892189189218198928";
+
+// num1 = parseFloat(num1); 
+
+// num1 = num1.toFixed(1);
+
+// // num1 = num1.toString()
+
+// console.log(typeof num1)
+// console.log(num1)
